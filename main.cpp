bc740a0aac6a032a9f50f9cdd8b905773d3fae14
rrizun <rrizun@rrizun-VirtualBox.(none)>
2013-02-28 19:16:31 -0500

    compiling/linking/running

--- main.cpp
+++ main.cpp
@@ -2,7 +2,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+extern "C" {
+#define __STDC_CONSTANT_MACROS
+#include <libavformat/avformat.h>
+//#include <libavcodec/avcodec.h>
+//#include <libavutil/avutil.h>
+}
+
 int main(int argc, char **argv) {
-	printf("Hello, world!\n");
+
+	av_register_all();
+
+	printf("Hello, world! %s\n", avformat_license());
+	printf("Hello, world! %s\n", avformat_configuration());
 	return 0;
 }
